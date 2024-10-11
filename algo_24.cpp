5a8e9b908d2933f5f6e2034003db1adcab0828d6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-11 17:48:55 +0300

    Create algo_24.cpp

---
+++ algo_24.cpp
@@ -0,0 +1,17 @@
+//std::set_intersection
+
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+
+
+int main()
+{
+	std::vector x{ 2, 3, 3, 7, 7, 19, 23, 31, 37, 45, 45, 61 };
+	std::vector y{ 2, 5, 7, 7, 9, 23, 32, 45, 57, 61, 63 };
+
+	std::set_intersection(x.begin(), x.end(),
+		y.begin(), y.end(), std::ostream_iterator<int>{std::cout, " "});
+}
