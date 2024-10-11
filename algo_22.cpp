fb74605bbd6c78616a0a1d62a6afa11bdda0e863
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-11 16:46:33 +0300

    Create algo_22.cpp

---
+++ algo_22.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+
+int main()
+{
+	std::vector vec{ 2, 2, 2, 5, 5, 5, 7, 9, 9, 9, 9, 12, 12, 16, 35 };
+	copy(begin(vec), end(vec), std::ostream_iterator<int>(std::cout, " "));
+
+	std::cout << "\nenter an integer: ";
+  int i;
+	std::cin >> i;
+
+	auto [it_lower, it_upper] = equal_range(vec.begin(), vec.end(), i);
+	std::cout << "distance for equal range : " << distance(it_lower, it_upper) << '\n';
+	std::cout << "index for lowerbound : " << distance(vec.begin(), it_lower) << '\n';
+	std::cout << "index for upperbound : " << distance(vec.begin(), it_upper) << '\n';
+}
