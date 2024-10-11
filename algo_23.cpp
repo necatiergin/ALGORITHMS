9472ab1fefbd2a0e79aca00326b42df80dac5b62
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-11 13:29:49 +0300

    algo_17.cpp

---
+++ algo_23.cpp
@@ -0,0 +1,22 @@
+//generate_n & nth_element
+
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	constexpr int size = 10'000;
+
+	vector<int> ivec;
+	ivec.reserve(size);
+	mt19937 eng;
+	uniform_int_distribution dist{ 0, 100'000 };
+	generate_n(back_inserter(ivec), 10'000, [&] {return dist(eng); });
+	nth_element(ivec.begin(), ivec.begin() + size / 2, ivec.end());
+	
+	cout << "median value is : " << ivec[size / 2];
+}
