f4ee849780c8c7bfa7c16d72db99d34ba726e77a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-12 17:04:00 +0300

    Create algo_08.cpp

---
+++ algo_08.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	std::vector v{ 1, 3, 5, 8, 7, 9, 2, 3};
+	
+	auto iter = is_sorted_until(v.begin(), v.end());
+	std::cout << "sorted_range ends at idx " << 
+		iter - v.begin() << '\n';
+
+	std::cout << "the first unsorted value is " << *iter << '\n';
+}
