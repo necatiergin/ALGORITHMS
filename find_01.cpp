f80c9949ca2d29cdc368ab7bfb3146e2471341b1
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-06 18:50:29 +0300

    Update and rename algo_05.cpp to find_01.cpp

--- find_01.cpp
+++ find_01.cpp
@@ -5,9 +5,8 @@
 int main()
 {
 	std::vector v{ 2, 7, 1, 4, 6, 8, 3, 2, 9, 1, 7, 6 };
-	int key;
-
 	std::cout << "enter the searched integer: ";
+	int key;
 	std::cin >> key;
 
 	if (auto iter = find(begin(v), end(v), key); iter != v.end()) {
