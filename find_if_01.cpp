c1c494fae6a6f788dd639984d68e5052534430be
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-17 14:20:33 +0300

    Update find_if_01.cpp

--- find_if_01.cpp
+++ find_if_01.cpp
@@ -12,7 +12,7 @@ int main()
 	std::cout << "enter the divisor : ";
 	std::cin >> divisor;
 
-	if (auto iter = find_if(cbegin(v), cend(v), [divisor](int x)
+	if (auto iter = std::find_if(cbegin(v), cend(v), [divisor](int x)
 		{return x % divisor == 0; }); iter != v.end()) {
 		std::cout << "found value is " << *iter << " idx is " << iter - v.begin() << '\n';
 	}
