34677c6f3712b2dc06583de659d1d26ee7c2af6a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-26 09:18:19 +0300

    Create sample_01.cpp

---
+++ sample_01.cpp
@@ -0,0 +1,23 @@
+// std::sample C++17
+
+#include <string>
+#include <iostream>
+#include <random>
+#include <iterator>
+
+int main()
+{
+	std::string input{"0123456789"};
+	std::cout << "enter the distance foe the sample: ";
+	int dist;
+	std::cin >> dist;
+
+	std::mt19937 eng{ std::random_device{}()};
+	std::string str{};
+
+	for (int i = 0; i < 10; ++i) {
+		std::sample(input.begin(), input.end(), std::back_inserter(str), dist, eng);
+		std::cout << str << '\n';
+		str = {};
+	}
+}
