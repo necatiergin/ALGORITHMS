f1b401dbabdf0d659619818c4da58436735b3e0b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-05-16 17:54:45 +0300

    Create mismatch_01.cpp

---
+++ mismatch_01.cpp
@@ -0,0 +1,12 @@
+/*
+
+namespace std {
+    template<class InputIt1, class InputIt2>
+    constexpr pair<InputIt1, InputIt2> mismatch(InputIt1 first1, InputIt1 last1, InputIt2 first2);
+
+    template<class InputIt1, class InputIt2, class BinaryPredicate>
+    constexpr pair<InputIt1, InputIt2> mismatch(InputIt1 first1, InputIt1 last1, InputIt2 first2,
+                                                BinaryPredicate p);
+}
+
+*/
