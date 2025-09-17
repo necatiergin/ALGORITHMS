e11d92083cd608ac70ca7da61efbdb31bf0ce320
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-09-17 17:58:10 +0300

    Update inner_product_01.cpp

--- numeric/inner_product_01.cpp
+++ numeric/inner_product_01.cpp
@@ -4,13 +4,11 @@
 
 int main()
 {
-	using namespace std;
-
-	vector vec1 { 2, 5, 7, 6, 4 };
-	vector vec2 { 2, 1, 2, 7, 8 };
+	std::vector vec1{ 2, 5, 7, 6, 4 };
+	std::vector vec2{ 2, 1, 2, 7, 8 };
 	// 0 + 4 + 5 + 14 + 42 + 32
-	
-	auto result = inner_product(vec1.begin(), vec1.end(), vec2.begin(), 0);    
 
-	cout << "result = " << result << '\n';
+	auto result = inner_product(vec1.begin(), vec1.end(), vec2.begin(), 0);
+
+	std::cout << "result = " << result << '\n';
 }
