b3bb85b51c521e6252651cbf0cbd0bb681740fd5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-09-22 13:13:27 +0300

    Update partial_sum_01.cpp

--- numeric/partial_sum_01.cpp
+++ numeric/partial_sum_01.cpp
@@ -4,13 +4,11 @@
 
 int main()
 {
-	using namespace std;
-
-	vector svec{ 1, 3, 5, 7, 9, 11, 13, 15 };
-	vector<int> dvec(svec.size());
+	std::vector svec{ 1, 3, 5, 7, 9, 11, 13, 15 };
+	std::vector<int> dvec(svec.size());
 
 	partial_sum(svec.begin(), svec.end(), dvec.begin());
 
 	for (auto i : dvec)
-		cout << i << ' ';
+		std::cout << i << ' ';
 }
