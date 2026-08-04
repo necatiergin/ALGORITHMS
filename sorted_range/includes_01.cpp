49ce1b82f0225445d661f8df1a2a2f8672613f6e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2026-08-04 18:23:42 +0300

    Create includes_01.cpp

---
+++ sorted_range/includes_01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() 
+{
+    // Sorted container that represents the superset
+    std::vector<int> vec = { 1, 3, 5, 7, 9, 11 };
+
+    // Sorted container that represents the subset
+    std::vector<int> subset = { 3, 5, 9 };
+
+    // Check if all elements in subset are contained in vec
+    bool result = std::includes(vec.begin(), vec.end(),
+        subset.begin(), subset.end());
+
+    if (result) {
+        std::cout << "vec includes all elements of subset.\n";
+    }
+    else {
+        std::cout << "vec does NOT include all elements of subset.\n";
+    }
+}
